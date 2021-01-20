f8a44f8996fcd38cd418715663a43aee1418f2c1
zhuko <zhukov_ai@outlook.com>
2021-01-20 11:14:02 +0400

    сделали фон

--- __main__.py
+++ __main__.py
@@ -1,19 +1,26 @@
 import pygame, time
 from pygame import display, event, image
 
+pygame.init()
+okno = display.set_mode([1200, 800])
+
 
 def obrabotka_sobitiy():
     # ОБРАБОТКА СОБЫТИЙ
     spisok_sobitiy = event.get()
-
     for sobitie in spisok_sobitiy:
         if sobitie.type == pygame.QUIT:
             exit()
 
 
+def risovanie_kadra():
+    okno.blit(fon_gori, [0, 0])
+    pygame.display.flip()
+
+
 fon_gori = image.load("kartynky/fon gori.jpg")
 
-display.set_mode([1200, 800])
 while 20 == 20:
     time.sleep(1 / 60)
     obrabotka_sobitiy()
+    risovanie_kadra()
