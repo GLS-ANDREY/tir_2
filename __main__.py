83bd6826cb527ff31f5938460c25791b6091ab7f
zhuko <zhukov_ai@outlook.com>
2021-01-22 11:27:22 +0400

    начали делать кнопку настройки

--- __main__.py
+++ __main__.py
@@ -1,8 +1,8 @@
 import pygame, time
-from pygame import display, event, image
+from pygame import display, event, image, transform
 
 pygame.init()
-okno = display.set_mode([1200, 800])
+okno = display.set_mode([700, 800])
 
 
 def obrabotka_sobitiy():
@@ -11,14 +11,20 @@ def obrabotka_sobitiy():
     for sobitie in spisok_sobitiy:
         if sobitie.type == pygame.QUIT:
             exit()
+        if sobitie.type == pygame.MOUSEBUTTONDOWN:
+            if sobitie.button == 1:
+                print(sobitie.pos)
 
 
 def risovanie_kadra():
     okno.blit(fon_gori, [0, 0])
+    okno.blit(knopka_nastroiki, [0, 0])
     pygame.display.flip()
 
 
 fon_gori = image.load("kartynky/fon gori.jpg")
+knopka_nastroiki = image.load("kartynky/knopka_nastroiki.jpg")
+knopka_nastroiki = transform.scale(knopka_nastroiki, [74, 74])
 
 while 20 == 20:
     time.sleep(1 / 60)
