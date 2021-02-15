74132cc62764d19ad3ee651d5969f74d74fcb824
zhuko <zhukov_ai@outlook.com>
2021-02-15 12:36:10 +0400

    сделал выход из кнопок

--- __main__.py
+++ __main__.py
@@ -40,23 +40,25 @@ def risovanie_gl():
 
 def risovanie_nastroek():
     okno.fill([64, 64, 64])
+    okno.blit(OK, geil)
     pygame.display.flip()
 
 
 def risovanie_pushek():
     okno.fill([51, 51, 51])
+    okno.blit(OK, geil)
     pygame.display.flip()
 
 
 def sdelay_nazvanie_igri():
     f = pygame.font.SysFont("comicsansms", 100, False, True)  # создаём шрифт
-    gavs = f.render("attack", True, [43, 219, 133])  # делаем картинку из букв
+    gavs = f.render("attack", True, [255, 90, 0])  # делаем картинку из букв
     gavs = pygame.transform.rotate(gavs, 10)  # поворачиваем картинку
 
-    amber = f.render("Ball", True, [43, 219, 133])
+    amber = f.render("Ball", True, [255, 90, 0])
     amber = pygame.transform.rotate(amber, 10)
 
-    volt = pygame.Surface([500,500],pygame.SRCALPHA)
+    volt = pygame.Surface([500, 500], pygame.SRCALPHA)
 
     volt.blit(gavs, [100, 100])
     volt.blit(amber, [100, 50])
@@ -67,11 +69,16 @@ fon = image.load("kartynky/fon.png")
 fon = pygame.transform.scale(fon, [700, 800])
 knopka_nastroiki = image.load("kartynky/knopka_nastroiki.png")
 knopka_nastroiki = help.izmeni_kartinku(knopka_nastroiki, 74, 74, [0, 168, 243], 1)
-lou = pygame.Rect(0, 0, 74, 74)
+lou = pygame.Rect(10, 10, 74, 74)
 knopka_pushek = image.load("kartynky/knopka_pushek.png")
 knopka_pushek = help.izmeni_kartinku(knopka_pushek, 74, 74, [0, 168, 243], 1)
-kollet = pygame.Rect(85, 0, 74, 74)
+kollet = pygame.Rect(100, 10, 74, 74)
 ball_attack = sdelay_nazvanie_igri()
+OK = image.load("kartynky/OK.png")
+OK = help.izmeni_kartinku(OK, 80, 74, [0, 168, 243], 1)
+geil = pygame.Rect(0, 0, 80, 74)
+geil.centerx = 350
+geil.centery = 605
 ekran = "gl"
 
 while 20 == 20:
